f23a4410193867b88424c3b975c937dbf21f5834
Daiki Anjiki <daikianjiki@gmail.com>
2023-07-19 16:14:23 -0700

    added minor validations

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,12 +4,19 @@ export default function Form() {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [isDisabled, setDisabled] = useState(false)
     
     function onChange(e: ChangeEvent<HTMLInputElement>) {
         
         if (e.target.name === "username") {
             setUsername(e.target.value)
+            if (e.target.value.length > 4) {
+                setDisabled(true)
+            } else {
+                setDisabled(false)
+            }
         } else if (e.target.name === "password") {
+            if (e.target.value.length > 8)
             setPassword(e.target.value)
         }   
     }
@@ -17,13 +24,14 @@ export default function Form() {
         <>
             <h1>Form</h1>
             <label>Username: </label>
-            <input type="text" name="username" onChange={onChange}/>
+            <input type="text" name="username" onChange={onChange}/> <span> Username must be longer than 4 characters</span>
             <label>Password: </label>
-            <input type="text" name="password" onChange={onChange}/>
+            <input type="text" name="password" onChange={onChange}/> <span> Password must be longer than 8 characters</span>
 
             <p>Username: {username}</p>
             <p>Password: {password}</p>
+
+            <button disabled={!isDisabled}>Button</button>
         </>
     )
-
 }
